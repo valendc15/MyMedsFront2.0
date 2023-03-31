d4aa882ae80478dfb6acbb8d8890fdd88f5effe9
valen <valentino.dicapua@ing.austral.edu.ar>
2023-03-31 17:28:44 -0300

    fixed buggs

--- src/Components/MedicNavBar.js
+++ src/Components/MedicNavBar.js
@@ -1,8 +1,21 @@
+import Logo from "./MyMedsLogo.png"
+import { Link } from "react-router-dom";
+
+
+
 function MedicNavBar(){
     
 
     return(
-        <h1>Hola</h1>
+        <nav>
+            <img src={Logo} ></img>
+            <div>
+                <ul>
+                    <li><Link to="/search">Search Patient</Link></li>
+                    <li><Link to="/viewPatients">View Patients</Link></li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
--- src/Components/viewPatients.js
+++ src/Components/viewPatients.js
@@ -1,5 +1,7 @@
 import { useEffect, useState} from "react";
 import { toast } from "react-toastify";
+import React from "react";
+import { Button } from "bootstrap";
 
 
 function ViewPatients(){
@@ -8,38 +10,35 @@ function ViewPatients(){
     const[patientList, setPatinetList]=useState("")
     const medicId=localStorage.getItem("id")
 
-    useEffect(
-            fetch(`http://localhost:8080/patient/${medicId}`)
-            .then(response =>{
-                if (!response.ok){
-                    throw Error
-                }
-                return (response.json())
-            }).then((data)=>{
-                const patients=data.map(patient=>
-                    <div class="card" style="width: 18rem;">
-                    <div class="card-body">
-                    <h5 class="card-title">Name: {patient.username}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">ID:{patient.primarykey}</h6>
-                    </div>
-                    </div>
-                    )
-                    setPatinetList(patients)
-            }).catch(err=>{
-                toast.warning('Failed to show patients')
-            })
-        
-    )
-
-
-
-
+    function handleClick(){
+        fetch(`http://localhost:8080/patient/${medicId}`)
+        .then(response =>{
+            if (!response.ok){
+                throw Error
+            }
+            return (response.json())
+        }).then((data)=>{
+            const patients=data.map(patient=>
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">Name: {patient.username}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">ID:{patient.primarykey}</h6>
+                </div>
+                </div>
+                )
+                setPatinetList(patients)
+        }).catch(err=>{
+            toast.warning('Failed to show patients')
+        })
+    }
 
 
     return(
         <div>
             <h1>Registered Patients</h1>
+            <button onClick={handleClick}>Show Patients</button>
             {patientList}
+
         </div>
     )
 }
