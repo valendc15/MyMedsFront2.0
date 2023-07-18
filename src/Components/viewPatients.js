6041da33c6416c60f116288e5b8196ef53782377
valendc15 <valentino.dicapua@ing.austral.edu.ar>
2023-07-17 18:40:19 -0600

    added filters to viewPatients

--- src/Components/viewPatients.js
+++ src/Components/viewPatients.js
@@ -7,18 +7,24 @@ import { Link } from "react-router-dom";
 
 function ViewPatients() {
   const [patientList, setPatientList] = useState([]);
+  const [filteredPatientList, setFilteredPatientList] = useState([]);
   const medicId = sessionStorage.getItem("id");
   const token = sessionStorage.getItem("token");
   const navigate = useNavigate();
+  const [dniFilter, setDniFilter] = useState("");
+  const [nameFilter, setNameFilter] = useState("");
 
-  useEffect(()=>{
-    handleOnClick()
-},[])
+  useEffect(() => {
+    handleOnClick();
+  }, [dniFilter, nameFilter]);
 
   function handleOnClick() {
     fetch(`http://localhost:8080/doctor/listpatients/${medicId}`, {
       method: "GET",
-      headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     })
       .then((response) => {
         if (response.status === 401) {
@@ -30,23 +36,36 @@ function ViewPatients() {
       .then((data) => {
         if (data != null || data != undefined) {
           setPatientList(data);
+          filterPatients(data);
         } else {
           setPatientList([]);
+          setFilteredPatientList([]);
         }
       });
   }
-  
-  function goToInfo(dni,name){
-    sessionStorage.setItem('info', 1)
-    navigate('/patientInfo', {state:{ dni:dni, name:name}})
 
+  function filterPatients(data) {
+    const filteredPatients = data.filter(
+      (patient) =>
+        patient.dni.toString().includes(dniFilter.toString()) &&
+        patient.username.toLowerCase().includes(nameFilter.toLowerCase())
+    );
+    setFilteredPatientList(filteredPatients);
+  }
+
+  function goToInfo(dni, name) {
+    sessionStorage.setItem("info", 1);
+    navigate("/patientInfo", { state: { dni: dni, name: name } });
   }
 
   function dismisP(dni) {
     const token = sessionStorage.getItem("token");
     fetch(`http://localhost:8080/doctor/listpatients/${sessionStorage.getItem("id")}`, {
       method: "DELETE",
-      headers: { "content-type": "application/json", Authorization: `Bearer ${token}` },
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
       body: JSON.stringify(dni),
     })
       .then((result) => {
@@ -63,15 +82,34 @@ function ViewPatients() {
       });
   }
 
+  const changeDNI = (event) => {
+    setDniFilter(event.target.value);
+  };
+
+  const changeName = (event) => {
+    setNameFilter(event.target.value);
+  };
+
   return (
     <div>
       <MedicNavBar></MedicNavBar>
-      <h1 className="text-center">Registered Patients</h1>
-      <div className="d-flex justify-content-center"> {/* Updated: Center align button */}
-      </div>
+      <h1>Registered Patients</h1>
+      <input
+        type="number"
+        className="form-control"
+        onChange={changeDNI}
+        placeholder="Search by patient DNI"
+      />
+      <input
+        type="text"
+        className="form-control"
+        onChange={changeName}
+        placeholder="Search by patient Name"
+      />
+      <div className="d-flex justify-content-center mt-3"></div>
       <div className="row justify-content-center">
-        {patientList.length > 0 ? (
-          patientList.map((patient) => (
+        {filteredPatientList.length > 0 ? (
+          filteredPatientList.map((patient) => (
             <motion.div
               className="col-md-4 col-sm-6 mb-4"
               key={patient.dni}
@@ -83,7 +121,9 @@ function ViewPatients() {
                 <div className="card-body">
                   <h5 className="card-title">Name: {patient.username}</h5>
                   <h6 className="card-subtitle mb-2 text-muted">DNI: {patient.dni}</h6>
-                  <button className="btn btn-info float-start" onClick={()=> goToInfo(patient.dni, patient.username)} >Info</button>
+                  <button className="btn btn-info float-start" onClick={() => goToInfo(patient.dni, patient.username)}>
+                    Info
+                  </button>
                   <button className="btn btn-danger float-end" onClick={() => dismisP(patient.dni)}>
                     Dismiss Patient
                   </button>
@@ -92,8 +132,7 @@ function ViewPatients() {
             </motion.div>
           ))
         ) : (
-          <h2 className="text-center mt-5"></h2>
-
+          <h2 className="text-center mt-5">No patients found.</h2>
         )}
       </div>
     </div>
